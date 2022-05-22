8a15f34c81eac358399027207caf01af367703c6
Doarakko <2wua4nlyi@gmail.com>
2022-05-22 19:27:11 +0900

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# yashima
+
+Play music when a moving object is detected.
+
+## Requirements
+
+The following is the environment where execution has been confirmed.
+
+### Hardware
+
+- Raspberry Pi 4 Model B
+- USB Speaker
+- HC SR501
+- Jumper Wire(M/M)x3
+
+### Software
+
+- Python 3.7.3
+- RPi.GPIO 0.7.0
+- Pygame 1.9.4.post1
+
+## Usage
+
+### 1. Download code
+
+```sh
+git clone https://github.com/Doarakko/yashima
+cd yashima
+```
+
+### 2. Run
+
+```sh
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,48 @@
+import time
+import pygame.mixer
+import RPi.GPIO as GPIO
+
+SENSOR_PIN = 18
+MUSIC_PATH = 'sample.mp3'
+
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SENSOR_PIN, GPIO.IN)
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(MUSIC_PATH)
+
+    print('setup is successful')
+
+
+def loop():
+    while True:
+        if GPIO.input(SENSOR_PIN) == GPIO.HIGH:
+            print('status: on')
+
+            if not pygame.mixer.music.get_busy():
+                channel = pygame.mixer.music.play(1)
+                print('action: start')
+        else:
+            print('status: off')
+
+        time.sleep(2)
+        print()
+
+
+def destroy():
+    GPIO.cleanup()
+
+    pygame.mixer.quit()
+
+    print('destroy is successful')
+
+
+if __name__ == '__main__':
+    setup()
+
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
