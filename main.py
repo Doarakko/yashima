1b01df6ddc07ebddf570a38960cb5eb73c7a13ee
Doarakko <2wua4nlyi@gmail.com>
2022-05-22 19:31:36 +0900

    single quote to double

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame.mixer
 import RPi.GPIO as GPIO
 
 SENSOR_PIN = 18
-MUSIC_PATH = 'sample.mp3'
+MUSIC_PATH = "sample.mp3"
 
 
 def setup():
@@ -13,19 +13,19 @@ def setup():
     pygame.mixer.init()
     pygame.mixer.music.load(MUSIC_PATH)
 
-    print('setup is successful')
+    print("setup is successful")
 
 
 def loop():
     while True:
         if GPIO.input(SENSOR_PIN) == GPIO.HIGH:
-            print('status: on')
+            print("status: on")
 
             if not pygame.mixer.music.get_busy():
                 channel = pygame.mixer.music.play(1)
-                print('action: start')
+                print("action: start")
         else:
-            print('status: off')
+            print("status: off")
 
         time.sleep(2)
         print()
@@ -36,10 +36,10 @@ def destroy():
 
     pygame.mixer.quit()
 
-    print('destroy is successful')
+    print("destroy is successful")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup()
 
     try:
